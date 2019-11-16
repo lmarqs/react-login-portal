804eb433c40f817fafce78b25ba6737241caf395
lmarqs <marques.analista@gmail.com>
2019-11-16 17:26:28 -0300

    wrap app into provider

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+
+import { store } from "./helpers";
+import { App } from "./App";
+
+import { configureFakeAPI } from "./helpers";
+
+configureFakeAPI();
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+render(app, document.getElementById("app"));
